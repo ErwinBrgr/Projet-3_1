91ac772c903d851e9ecc2f77d3fc428cf797071c
ErwinBrgr <44338670+ErwinBrgr@users.noreply.github.com>
2018-11-26 17:57:11 +0100

    Add files via upload

--- javascript/leaflet.js
+++ javascript/leaflet.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([49.443232, 1.099971], 15);
+var mymap = L.map('mapid').setView([49.443232, 1.099971], 15); // stockage carte dans div avec gestion de la position dans la ville de Rouen
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -16,14 +16,14 @@ ajaxGet(urlapi, function (reponse) {
     console.log(stations);
     var LeafIcon = L.Icon.extend({
     options: {
-        iconSize:     [50, 50],
-        shadowSize:   [50, 64],
-        iconAnchor:   [1, 1],
-        shadowAnchor: [4, 62],
-        popupAnchor:  [-3, -76]
+        iconSize:     [50, 50], //taille de l'icône
+        shadowSize:   [50, 64], //ombre porté (à suppr pdt test)
+        iconAnchor:   [1, 1], //ancrage de l'icône
+        shadowAnchor: [4, 62],//ancrage de l'ombre (à suppr pdt test)
+        popupAnchor:  [-3, -76] //gestion pop plus d'actualité, à supprim après test
     }
 });
-
+ //création de variables pour stackage des 3 différentes icônes (verte, orange, rouge)
     var greenIcon = new LeafIcon({iconUrl: './images/marker_green.png'}),
     redIcon = new LeafIcon({iconUrl: './images/marker_red.png'}),
     orangeIcon = new LeafIcon({iconUrl: './images/marker_orange.png'});
@@ -35,41 +35,43 @@ ajaxGet(urlapi, function (reponse) {
 
 //Insertion des markers sur la carte
     stations.forEach(function (station) {
-          //Méthode pour mise en forme des noms des stations
+          //Méthode pour mise en forme des noms des stations (à retravailler)
 
-            var tableau_name = station.name.split("-");
+            var tableau_name = station.name.split("-"); //
           	var station_name = tableau_name[1] ;
 
          //Choix des markers selons statuts des stations et nombres de vélos présents dans la station
 
+            //condition pour couleur du marker. si nb vélo sup à 5 afichage du markeur en vert
         if (station.available_bikes >= 5) {
         var marker =L.marker([station.position.lat, station.position.lng], {icon: greenIcon}).addTo(mymap);
-         }
+         }//idement mais affichage en orange
         else if(station.status = "OPEN" && station.available_bikes <= 4 ) {
         var marker =L.marker([station.position.lat, station.position.lng], {icon: orangeIcon}).addTo(mymap);
-        }else if (station.available_bikes === 0) {
+        }else if (station.available_bikes === 0) { //si 0 marker eu rouge
         var marker =L.marker([station.position.lat, station.position.lng], {icon: redIcon}).addTo(mymap);
         }
-        else {
+        else { //si aucune conditon remplie : rouge
         var marker =L.marker([station.position.lat, station.position.lng], {icon: redIcon}).addTo(mymap);
         };
 
   		//Méthode d'insertion des informations dans div infoStation
 
 		displayPanel = function(){
-		$("#mapid").width("70%");
-		$("#infoStation").show();
-		$("#nomStation").html(station.name);
-		$("#etatStation").html(station.status);
-		$("#veloDispo").html(station.available_bikes);
-		$("#attacheDispo").html(station.available_bike_stands);
+		$("#mapid").width("70%"); //changement de la largeur de carte pour 70% de la taille de la page
+		$("#infoStation").show(); //affichage div en display none par défault
+		$("#nomStation").html(station.name); //affichage du nom de la station
+		$("#etatStation").html(station.status); //affichage statut open ou close de la station
+		$("#veloDispo").html(station.available_bikes); //affichage de nombre de de vélos disponible
+		$("#attacheDispo").html(station.available_bike_stands); //affichage du nombre d'attache dispo
 
 		};
 
-marker.on('click', displayPanel);
+marker.on('click', displayPanel); //gestion du click sur le marker pour affichage des informations (via fonction display)
 });
 
 
+//gestion du clic sur le bouton "valider". Affiche une zone avec information sur la station et un countdown de 20 mins
    var btnValid = $('#validationCanvas');
         afficheInfo = function(){
         $("#selectionStation").show();
@@ -77,7 +79,7 @@ marker.on('click', displayPanel);
         decompte(1000);
         };
 
-      btnValid.on('click',afficheInfo);
+      btnValid.on('click',afficheInfo); //trigger sur bouton valider pour déclangement des informations de la station
     });
 
 
